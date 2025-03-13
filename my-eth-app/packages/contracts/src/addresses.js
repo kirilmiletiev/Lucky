5a7eab9f112ecf1dfe09b230d5b658941f79e3df
Kiril Miletiev <kirilmiletiev@gmail.com>
2025-03-14 00:51:54 +0200

    .

--- my-eth-app/packages/contracts/src/addresses.js
+++ my-eth-app/packages/contracts/src/addresses.js
@@ -1,6 +1,6 @@
 // This address points to a dummy ERC-20 contract. Replace it with your own smart contracts.
 const addresses = {
-//  ceaErc20: "0xa6dF0C88916f3e2831A329CE46566dDfBe9E74b7",
- ceaErc20: "",
+    ceaErc20: "0xa6dF0C88916f3e2831A329CE46566dDfBe9E74b7",
+    //ceaErc20: "",
 };
 export default addresses;
